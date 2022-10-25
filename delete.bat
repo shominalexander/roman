db4180f83b373aec358833b9614231346548326f
shominalexander <40673785+shominalexander@users.noreply.github.com>
2022-10-25 08:47:16 +0300

    Add files via upload

---
+++ delete.bat
@@ -0,0 +1,15 @@
+echo off
+
+cls
+
+echo delete Cargo.lock file and target folder? ( y - yes, other - no )
+
+set /p delete=
+
+if [%delete%] == [y] (
+ del Cargo.lock
+
+ rmdir /s/q target
+
+ pause
+)
